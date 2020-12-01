b3f9896ca85fbb77d834d0fa249536d2d2295c1a
IOsetting <iosetting@outlook.com>
2020-12-01 21:15:03 +0800

    Initialize with v1.0.0

---
+++ src/translations.js
@@ -0,0 +1,26 @@
+var L = {
+	'en' : {
+		'IPFS Gateway Selector' : 'IPFS Gateway Selector',
+		'Reload Gateways' : 'Reload Gateways',
+		'Configurations' : 'Configurations',
+		'Gateway Source' : 'Gateway Source',
+		'Probe CID' : 'Probe CID',
+		'Timeout' : 'Timeout',
+		'History' : 'History',
+		'Clear' : 'Clear',
+		'Gateways' : 'Gateways',
+		'Retest' : 'Retest'
+	},
+	'zh-cn' : {
+		'IPFS Gateway Selector' : 'IPFS下载网关选择',
+		'Reload Gateways' : '刷新下载列表',
+		'Configurations' : '配置',
+		'Gateway Source' : '网关来源',
+		'Probe CID' : '探测CID',
+		'Timeout' : '超时',
+		'History' : '访问历史',
+		'Clear' : '清空',
+		'Gateways' : '网关列表',
+		'Retest' : '重新测速'
+	}
+};
